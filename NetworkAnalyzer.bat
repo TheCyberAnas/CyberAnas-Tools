b401b0ad09fab86321545a40c595a1378de9c0e6
CyberAnas <253500177+TheCyberAnas@users.noreply.github.com>
2026-01-07 14:43:37 +0100

    Create NetworkAnalyzer.bat

---
+++ NetworkAnalyzer.bat
@@ -0,0 +1,21 @@
+@echo off
+title NETWORK ANALYZER - @thecyberanas
+color 0a
+cls
+echo ==================================================
+echo           SYSTEM ANALYSIS BY THECYBERANAS
+echo ==================================================
+echo.
+echo [+] ANALISI IP LOCALE...
+ipconfig | findstr "IPv4"
+echo.
+echo [+] TEST LATENZA (GOOGLE PING)...
+ping google.com -n 3
+echo.
+echo [+] INFO SCHEDA DI RETE...
+getmac
+echo.
+echo ==================================================
+echo Scarica altri script su: github.com/thecyberanas
+echo ==================================================
+pause
